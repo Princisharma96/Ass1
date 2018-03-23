2443c0a1091465ad7cfb07d45fcea21782050245
Princisharma96 <princisharma96@gmail.com>
2018-03-23 07:04:27 +0530

    my final commit

---
+++ main.py
@@ -0,0 +1,71 @@
+######OBJECTIVE 1#########
+
+print("WELCOME TO SPYCHAT")
+
+spy_name=input("What's your spy name") #taking the spy name as input by the user#
+
+if len(spy_name)>0:
+
+    spy_salutation = input("What should we call you (Mr. or Ms.)?")  ##taking the salutation as input by the user#
+
+    spy_name=spy_salutation+ ""+ spy_name #concatenating the salutation and the spy name#
+
+    print("Alright "+ spy_name +" I'd like to know a little bit more about you...")  #printing the final statement#
+
+else:
+    print("Please provide us with your name first!")
+    # valid entry
+
+
+#####OBJECTIVE 2##########
+
+print("SPYCHAT")
+
+default_user=input("Do you wish to continue with the default user(Y or N)? ,If No create a new user")
+
+# press 'Y' for the default user & 'N' to create a new user #
+
+# for default user #
+if default_user=='Y'or default_user=='y':
+    print("continue")
+    import spy_details    ##import the spy details from another spy_details file #
+
+
+# for a new user #
+elif default_user=='N' or default_user=='n':
+    # asking new spy name
+    spy_name = input("What's your name?")
+    # checking length of the name
+    if len(spy_name) > 0:
+        # asking for spy salutation
+        spy_salutation = input("What would you like us to call you (Mr., Ms. or Mrs.) ?")
+        # welcoming spy
+        print("Welcome to the spy chat " + spy_salutation + " " + spy_name)
+        print("Alright " + spy_salutation + " " + spy_name + " I'd like to know a little bit more about you...")
+        # checking spy age
+        spy_age = int(input("What's your age?"))
+        if spy_age > 12 and spy_age < 50:
+            # checking spy rating
+            spy_rating = float(input("What is your spy rating?"))
+            if spy_rating > 4.5:
+                print("Outstanding!")
+            elif spy_rating > 3.5 and spy_rating <= 4.5:
+                print("Amazing!")
+            elif spy_rating >= 2.5 and spy_rating <= 3.5:
+                print("You can surely improve!")
+            else:
+                print("Don't Worry! We'll help you!")
+            # if spy is online
+            spy_is_online = True
+            # welcome with details
+            print("Authentication complete. Welcome " + spy_name + " age: " + str(spy_age) + " and rating of: " + str(spy_rating) + " Proud to have you onboard")
+        # age is not eligible
+        else:
+            print("You are not ready to be a spy yet!")
+    # name not provided
+    else:
+        print("Please provide us with your name first!")
+    # valid entry
+else:
+    print("Please enter default or create.")
+
